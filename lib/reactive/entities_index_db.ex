5f8aa62d8c4ac594f5f3e5d7ea638509fa2e27ed
Michał Łaszczewski <michal@cloudforge.pl>
2015-08-28 15:33:51 +0200

    fixed bugs

--- lib/reactive/entities_index_db.ex
+++ lib/reactive/entities_index_db.ex
@@ -11,14 +11,15 @@ defmodule Reactive.EntitiesIndexDb do
   def add({db,index_name},key,entity_id) do
     dbId=Reactive.EntitiesDb.entity_db_id(entity_id)
     keyPrefix="ei:" <> index_name <> ":i:" <> key <> ":"
-    Reactive.Db.put(db,keyPrefix <> dbId,dbId)
+    Reactive.Db.put(db,keyPrefix <> dbId,:erlang.term_to_binary(entity_id))
   end
   def find({db,index_name},key) do
     keyPrefix="ei:" <> index_name <> ":i:" <> key <> ":"
-    Reactive.Db.scan(db,%{
+    values=Reactive.Db.scan(db,%{
       prefix: keyPrefix,
       fetch: :value
     })
+    Enum.map(values,fn(b) -> :erlang.binary_to_term(b) end)
   end
   def delete({db,index_name},key,entity_id) do
     dbId=Reactive.EntitiesDb.entity_db_id(entity_id)
